a532f4db5c0f014423e2225fd0a24975e3d434fd
huntersair <78912380+huntersair@users.noreply.github.com>
2026-05-23 20:15:21 +0100

    adapting for variable-length sequences in preparation to incorporate MPRA data
    As CNNs require consistent tensor shapes padding has been included such that if the input sequence length is less than 200 it is padded with [0,0,0,0] until 200 rows are reached. Otherwise, when sequence length is above 200, it is truncated.

--- src/datasets.py
+++ src/datasets.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import torch
+import numpy as np
 
 from torch.utils.data import Dataset
 
@@ -8,9 +9,15 @@ from src.utils import one_hot_encode
 
 class SequenceDataset(Dataset):
 
-    def __init__(self, csv_path: str):
+    def __init__(
+        self,
+        csv_path: str,
+        max_length: int = 200
+    ):
 
         self.dataframe = pd.read_csv(csv_path)
+
+        self.max_length = max_length
 
     def __len__(self):
 
@@ -25,6 +32,10 @@ class SequenceDataset(Dataset):
 
         encoded_sequence = one_hot_encode(sequence)
 
+        encoded_sequence = self.pad_sequence(
+            encoded_sequence
+        )
+
         sequence_tensor = torch.tensor(
             encoded_sequence,
             dtype=torch.float32
@@ -36,3 +47,30 @@ class SequenceDataset(Dataset):
         )
 
         return sequence_tensor, expression_tensor
+
+    def pad_sequence(self, encoded_sequence):
+
+        sequence_length = encoded_sequence.shape[0]
+
+        if sequence_length > self.max_length:
+
+            encoded_sequence = encoded_sequence[
+                :self.max_length
+            ]
+
+        elif sequence_length < self.max_length:
+
+            padding_length = (
+                    self.max_length - sequence_length
+            )
+
+            padding = np.zeros(
+                (padding_length, 4),
+                dtype=np.float32
+            )
+
+            encoded_sequence = np.vstack(
+                [encoded_sequence, padding]
+            )
+
+        return encoded_sequence
